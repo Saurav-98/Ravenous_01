3b3a242aee983b7efe552284070d17135e2a59d5
Saurav <sauravkumarverma22@gmail.com>
2022-04-25 03:02:15 +0530

    ravenous
    Ravenous Project

---
+++ src/components/BusinessList/BusinessList.jsx
@@ -0,0 +1,18 @@
+import "./BusinessList.css";
+import React from "react";
+import Business from "../Business/Business";
+
+function BusinessList() {
+  return (
+    <div class="BusinessList">
+      <Business className="" />
+      <Business className="" />
+      <Business className="" />
+      <Business className="" />
+      <Business className="" />
+      <Business className="" />
+    </div>
+  );
+}
+
+export default BusinessList;
